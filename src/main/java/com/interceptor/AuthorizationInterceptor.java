fcf0e41540e1845948d93cbe4485543194562b1b
buzhiyanchou <508805622@qq.com>
2022-05-27 20:35:58 +0800

    登錄攔截恢復

--- src/main/java/com/interceptor/AuthorizationInterceptor.java
+++ src/main/java/com/interceptor/AuthorizationInterceptor.java
@@ -71,18 +71,17 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
         	return true;
         }
         
-//		PrintWriter writer = null;
-//		response.setCharacterEncoding("UTF-8");
-//		response.setContentType("application/json; charset=utf-8");
-//		try {
-//		    writer = response.getWriter();
-//		    writer.print(JSONObject.toJSONString(R.error(401, "请先登录")));
-//		} finally {
-//		    if(writer != null){
-//		        writer.close();
-//		    }
-//		}
-//				throw new EIException("请先登录", 401);
-		return true;
+		PrintWriter writer = null;
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json; charset=utf-8");
+		try {
+		    writer = response.getWriter();
+		    writer.print(JSONObject.toJSONString(R.error(401, "请先登录")));
+		} finally {
+		    if(writer != null){
+		        writer.close();
+		    }
+		}
+				throw new EIException("请先登录", 401);
     }
 }
